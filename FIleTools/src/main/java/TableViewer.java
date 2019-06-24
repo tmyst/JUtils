cdc5dbd459e9204b92e29c0d397fb539ca7136a0
tmyst <tomoya.saitoh@gmail.com>
2019-06-25 01:36:43 +0900

    First Commit

---
+++ FIleTools/src/main/java/TableViewer.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TableViewer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage){
+
+        Scene scene = new Scene(new Group(), 600, 800);
+
+
+    }
+}

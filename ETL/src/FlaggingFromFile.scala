89234389e6ea800738ecafd20917e84cbcd79ea9
tmyst <tomoya.saitoh@gmail.com>
2019-07-01 01:43:20 +0900

    Add Spark

--- ETL/src/FlaggingFromFile.scala
+++ ETL/src/FlaggingFromFile.scala
@@ -1,3 +1,12 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 object FlaggingFromFile {
+  val dataFilePath = "D:\\data\\Toy\\toy.csv"
+  val flagFilePath = "D:\\data\\Toy\\flag.csv"
+
+  val sc = new SparkConf().setMaster("local[*]").setAppName("Flagging")
+  val ss = SparkSession.builder().config(sc).config("spark.local.dir", "D:/tmp").config("spark.driver.memory", "10g").getOrCreate()
+
 
 }
